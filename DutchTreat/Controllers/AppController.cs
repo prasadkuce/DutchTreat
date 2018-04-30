5e87abb674636760dd2ea3adf95522f8e4832b8e
Prasad <prasadkuce@live.in>
2018-04-30 16:28:34 +0530

    Add project files.

---
+++ DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,43 @@
+﻿using DutchTreat.Services;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController:Controller
+    {
+        private readonly IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                _mailService.SendMessage("Me", model.Subject, model.Message);
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear();
+            }
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
